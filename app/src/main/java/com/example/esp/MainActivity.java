85bdefd835c3a42fd0996c29ee14e024a90c25c4
Guendouz Moulkhere <gmoulkhere@gmail.com>
2024-02-11 18:58:32 -0500

    First

---
+++ app/src/main/java/com/example/esp/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.esp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    setContentView(R.layout.activity_main2);
+    setContentView(R.layout.donnez);
+    setContentView(R.layout.donnez1);
+    setContentView(R.layout.login);
+    setContentView(R.layout.login1);
+    setContentView(R.layout.rdv);
+    setContentView(R.layout.rdv1);
+}}
