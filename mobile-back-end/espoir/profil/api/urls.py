6f82d9b10557edc14da4d3d37854a11836c943b8
massinissa chemani <@chemani.massinissa>
2024-02-18 20:40:51 -0500

    ajout de login

--- mobile-back-end/espoir/profil/api/urls.py
+++ mobile-back-end/espoir/profil/api/urls.py
@@ -1,10 +1,12 @@
 from rest_framework import routers
-from .views import ProfilViewSet
+from .views import ProfilViewSet,LoginView
 from django.urls import path,include
 
 router = routers.DefaultRouter()
 router.register(r'profil',ProfilViewSet)
 
+
 urlpatterns = [
-    path('',include(router.urls))
+    path('',include(router.urls)),
+    path('login/', LoginView.as_view(), name='login')
 ]
--- mobile-back-end/espoir/profil/api/views.py
+++ mobile-back-end/espoir/profil/api/views.py
@@ -1,7 +1,26 @@
 from rest_framework import viewsets
 from ..models import Profile
 from .serializers import ProfileSerializer
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import authenticate, login
 
 class ProfilViewSet(viewsets.ModelViewSet):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
+    
+class LoginView(APIView):
+    def post(self, request, *args, **kwargs):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({'token': token.key, 'user_id': user.id}, status=status.HTTP_200_OK)
+        else:
+            return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
